419aa5dcc0e859ac77544a73dac5f33bc71deedf
Moyo <moyorodriguez567@gmail.com>
2018-10-19 12:15:44 -0600

    Se agrego el proyecto UI y el ribbonform de devex, con todas sus dependencias

---
+++ UI/frmPrincipal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraBars;
+
+namespace UI
+{
+    public partial class frmPrincipal : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+    }
+}
