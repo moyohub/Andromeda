082bab86d1f0a25f4aafc01bcee6620744a4468a
Moyo <moyorodriguez567@gmail.com>
2018-10-18 21:34:33 -0600

    First Commit
    First Commit

---
+++ BLL/BLLClass.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using MySql.Data.MySqlClient;
+using DAL;
+
+namespace BLL
+{
+    public class BLLClass
+    {
+        DataTable vdtProcedureItems;
+        DALClass DC;
+
+        public BLLClass()
+        {
+            DC = new DALClass("mysql5006.site4now.net", "a41be6_andro01", "wording", "root1234");
+            //DC = new DALClass("localhost", "apq", "root", "");
+
+        }
+
+        public void ProcedimientoAlmacenado(string ProcedimientoAlmacenado, params object[] parametros)
+        {
+            List<MySqlParameter> parametrosentrada = new List<MySqlParameter>();
+            List<MySqlParameter> parametrossalida = new List<MySqlParameter>();
+            vdtProcedureItems = new DataTable();
+
+            vdtProcedureItems = DC.Sql("select", "information_schema.parameters", "PARAMETER_NAME", "SPECIFIC_NAME = '" + ProcedimientoAlmacenado + "' and PARAMETER_MODE = 'IN' and SPECIFIC_SCHEMA = 'db_a299f8_aqp';");
+            //vdtProcedureItems = DC.Sql("select", "information_schema.parameters", "PARAMETER_NAME", "SPECIFIC_NAME = '" + ProcedimientoAlmacenado + "' and PARAMETER_MODE = 'IN' and SPECIFIC_SCHEMA = 'apq';");
+
+            if (Convert.ToInt32(parametros.Count()) == vdtProcedureItems.Rows.Count)
+            {
+                for (int i = 0; i < parametros.Count(); i++)
+                {
+                    parametrosentrada.Add(new MySqlParameter(vdtProcedureItems.Rows[i]["PARAMETER_NAME"].ToString(), parametros[i].ToString()));
+                }
+                parametrossalida.Add(new MySqlParameter("$respuesta", ""));
+                DC.ProcedimientoAlmacenado(ProcedimientoAlmacenado, parametrosentrada, parametrossalida);
+                MessageBox.Show(parametrossalida[0].Value.ToString());
+            }
+            else
+            {
+                MessageBox.Show("El numero de parametros no es el correcto");
+            }
+        }
+
+        public DataTable Consulta(string ProcedimientoAlmacenado, params object[] parametros)
+        {
+            List<MySqlParameter> parametrosentrada = new List<MySqlParameter>();
+            List<MySqlParameter> parametrossalida = new List<MySqlParameter>();
+
+            for (int i = 0; i < parametros.Count(); i++)
+            {
+                parametrosentrada.Add(new MySqlParameter("$para" + i, parametros[i].ToString()));
+            }
+
+            return DC.Consulta(ProcedimientoAlmacenado, parametrosentrada);
+        }
+    }
+}
---
+++ DAL/DALClass.cs
@@ -0,0 +1,264 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace DAL
+{
+    public class DALClass
+    {
+        string cadenaconexion = "";
+        MySqlConnection conexion;
+
+        public DALClass(string server, string basededatos, string usuario, string clave)
+        {
+            cadenaconexion = "Server=" + server + "; Uid=" + usuario + "; Pwd=" + clave + "; Database=" + basededatos + ";";
+            conexion = new MySqlConnection(cadenaconexion);
+        }
+
+        public DataTable ProcedimientoAlmacenado(string procedimiento, List<MySqlParameter> parametrosentrada, List<MySqlParameter> parametrossalida)
+        {
+            if (conexion != null)
+            {
+                DataTable resultado = new DataTable();
+                MySqlDataAdapter adaptador;
+                MySqlCommand comando = new MySqlCommand();
+                try
+                {
+                    conexion.Open();
+                    comando.Connection = conexion;
+                    comando.CommandText = procedimiento;
+                    comando.CommandType = CommandType.StoredProcedure;
+                    foreach (MySqlParameter parametro in parametrosentrada)
+                    {
+                        parametro.Direction = ParameterDirection.Input;
+                        comando.Parameters.Add(parametro);
+                    }
+                    foreach (MySqlParameter parametro in parametrossalida)
+                    {
+                        parametro.Direction = ParameterDirection.Output;
+                        comando.Parameters.Add(parametro);
+                    }
+                    adaptador = new MySqlDataAdapter(comando);
+                    adaptador.Fill(resultado);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                finally
+                {
+                    conexion.Close();
+                }
+                return resultado;
+            }
+            return new DataTable();
+        }
+
+        public DataTable Consulta(string procedimiento, List<MySqlParameter> parametrosentrada)
+        {
+            if (conexion != null)
+            {
+                DataTable resultado = new DataTable();
+                MySqlDataAdapter adaptador;
+                MySqlCommand comando = new MySqlCommand();
+                try
+                {
+                    conexion.Open();
+                    comando.Connection = conexion;
+                    comando.CommandText = procedimiento;
+                    comando.CommandType = CommandType.StoredProcedure;
+                    foreach (MySqlParameter parametro in parametrosentrada)
+                    {
+                        parametro.Direction = ParameterDirection.Input;
+                        comando.Parameters.Add(parametro);
+                    }
+                    adaptador = new MySqlDataAdapter(comando);
+                    adaptador.Fill(resultado);
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                finally
+                {
+                    conexion.Close();
+                }
+                return resultado;
+            }
+            return new DataTable();
+        }
+
+
+        public DataTable Sql(string comandotexto, string tabla, List<String> campos, string condicion)
+        {
+            if (conexion != null)
+            {
+                DataTable resultado = new DataTable();
+                MySqlDataAdapter adaptador;
+                MySqlCommand comando = new MySqlCommand();
+                comando = new MySqlCommand();
+                comando.Connection = conexion;
+                comando.CommandType = CommandType.Text;
+                try
+                {
+                    conexion.Open();
+                    string sql;
+                    switch (comandotexto)
+                    {
+                        case "insert":
+                            sql = "insert into " + tabla + " values(";
+                            SumarCampos(ref sql, campos);
+                            sql += ")";
+                            comando.CommandText = sql;
+                            comando.Connection = conexion;
+                            adaptador = new MySqlDataAdapter(comando);
+                            adaptador.Fill(resultado);
+                            break;
+                        case "delete":
+                            sql = "delete from " + tabla + " where " + condicion;
+                            comando.CommandText = sql;
+                            comando.Connection = conexion;
+                            adaptador = new MySqlDataAdapter(comando);
+                            adaptador.Fill(resultado);
+                            break;
+                        case "update":
+                            sql = "update " + tabla + " set ";
+                            SumarCampos(ref sql, campos);
+                            sql += " where " + condicion;
+                            comando.CommandText = sql;
+                            comando.Connection = conexion;
+                            adaptador = new MySqlDataAdapter(comando);
+                            adaptador.Fill(resultado);
+                            break;
+                        case "select":
+                            sql = "select ";
+                            SumarCampos(ref sql, campos);
+                            sql += " from " + tabla;
+                            if (condicion != "")
+                                sql += " where " + condicion;
+                            comando.CommandText = sql;
+                            adaptador = new MySqlDataAdapter(comando);
+                            adaptador.Fill(resultado);
+                            break;
+                        case "select top":
+                            sql = "select top 1";
+                            SumarCampos(ref sql, campos);
+                            sql += " from " + tabla;
+                            if (condicion != "")
+                                sql += " where " + condicion;
+                            comando.CommandText = sql;
+                            adaptador = new MySqlDataAdapter(comando);
+                            adaptador.Fill(resultado);
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                finally
+                {
+                    conexion.Close();
+                }
+                return resultado;
+            }
+            return new DataTable();
+        }
+
+        public DataTable Sql(string comandotexto, string tabla, string campos, string condicion)
+        {
+            if (conexion != null)
+            {
+                DataTable resultado = new DataTable();
+                MySqlDataAdapter adaptador;
+                MySqlCommand comando = new MySqlCommand();
+                comando = new MySqlCommand();
+                comando.Connection = conexion;
+                comando.CommandType = CommandType.Text;
+                try
+                {
+                    conexion.Open();
+                    string sql;
+                    switch (comandotexto)
+                    {
+                        case "select":
+                            sql = "select " + campos;
+                            sql += " from " + tabla;
+                            if (condicion != "")
+                                sql += " where " + condicion;
+                            comando.CommandText = sql;
+                            adaptador = new MySqlDataAdapter(comando);
+                            adaptador.Fill(resultado);
+                            break;
+                        case "select top":
+                            sql = "select " + campos;
+                            sql += " from " + tabla;
+                            if (condicion != "")
+                                sql += " where " + condicion;
+                            comando.CommandText = sql;
+                            adaptador = new MySqlDataAdapter(comando);
+                            adaptador.Fill(resultado);
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message);
+                }
+                finally
+                {
+                    conexion.Close();
+                }
+                return resultado;
+            }
+            return new DataTable();
+        }
+
+        private void SumarCampos(ref string sql, List<string> campos)
+        {
+            int numerocampos = campos.Count;
+            for (int x = 0; x < numerocampos - 1; x++)
+            {
+                sql += campos[x] + ",";
+            }
+            sql += campos[numerocampos - 1];
+        }
+
+        public DataTable EjecutarSql(string sql)
+        {
+            try
+            {
+
+                conexion.Open();
+                MySqlCommand comando = new MySqlCommand(sql, conexion);
+                comando.CommandType = CommandType.Text;
+                MySqlDataAdapter adaptador = new MySqlDataAdapter(comando);
+                DataTable resultado = new DataTable();
+                adaptador.Fill(resultado);
+                return resultado;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+            finally
+            {
+                conexion.Close();
+            }
+        }
+
+
+        ~DALClass()
+        {
+            if (conexion != null)
+                conexion.Close();
+        }
+    }
+}
